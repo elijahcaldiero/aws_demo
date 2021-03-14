561cc4021ac0c4bf5966f13f0b05d22e195e40f9
Elijah Caldiero <elijah.caldiero@gmail.com>
2021-03-13 21:09:54 -0700

    Hello world

--- main.tf
+++ main.tf
@@ -1,25 +1,24 @@
 provider "aws" {
-  profile = "default"
+  profile = "default" 
   region = "us-east-2"
 }
 
 variable "server_port" {
 description = "The port the server will use for HTTP requests"
-default = 80
+default = 8080
 }
 
 
 resource "aws_launch_configuration" "test" {
-  image_id = "ami-02ad6b83fd606d009"
+  image_id = "ami-04370661"
   instance_type = "t2.micro"
   security_groups = [aws_security_group.instance.id]
 
+
   user_data = <<-EOF
               #!/bin/bash
-	      sudo yum -q -y install httpd git
-              git clone https://github.com/elijahcaldiero/aws_demo.git
-              sudo ln -s /home/centos/aws_demo/webapp /var/www/html
-              sudo systemctl start httpd
+              echo "Hello World" > index.html
+              nohup busybox httpd -f -p var.server_port &
               EOF
 
 lifecycle {
@@ -37,13 +36,6 @@ resource "aws_security_group" "instance" {
     from_port = var.server_port
     to_port = var.server_port
     protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
@@ -82,14 +74,14 @@ resource "aws_elb" "test" {
     instance_port = var.server_port
     instance_protocol = "http"
   }
-#
-#health_check {
-#  healthy_threshold = 1
-#  unhealthy_threshold = 600
-#  timeout = 2
-#  interval = 1
-#  target = "HTTP:${var.server_port}/"
-# }
+
+health_check {
+  healthy_threshold = 2
+  unhealthy_threshold = 2
+  timeout = 3
+  interval = 30
+  target = "HTTP:${var.server_port}/"
+ }
 }
 
 resource "aws_security_group" "elb" {
@@ -101,7 +93,7 @@ resource "aws_security_group" "elb" {
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
+ 
   egress {
     from_port = 0
     to_port = 0
@@ -109,18 +101,3 @@ resource "aws_security_group" "elb" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
-
-#resource "aws_db_instance" "testdb"{
-#  allocated_storage = 5
-#  engine = "mysql"
-#  engine_version = "5.7"
-#  instance_class = "db.t2.micro"
-#  name = "demodb"
-#  username = "demouser"
-#  password = random_string.dbpass.result
-#  skip_final_snapshot = true
-#}
-#
-#resource "random_string" "dbpass"{
-#  length = 16
-#}
